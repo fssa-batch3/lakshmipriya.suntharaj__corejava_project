8d02cd2d0e46fb7ccfda6790e0c9590073a0e8f3
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-07-12 10:09:30 +0530

    day02

--- src/main/java/day2/practice/Logger.java
+++ src/main/java/day2/practice/Logger.java
@@ -2,4 +2,19 @@ package day2.practice;
 
 public class Logger {
 
+	public static void debug (String msg) {
+		System.out.println("DEBUG: " + msg);
+	}
+	public static void info (String msg) {
+		// TODO complete this code using the above template
+		System.out.println();
+	}
+	
+	public static void error (String msg) {
+		// TODO complete this code using the above template
+	}
+	
+	public static void debug (int num) {
+		// TODO Complete this code
+	}
 }
