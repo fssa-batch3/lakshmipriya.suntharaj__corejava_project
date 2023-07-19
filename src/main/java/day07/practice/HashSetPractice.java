0ab7d863b689f56f98cf15129707beb2f627791c
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-07-19 06:50:53 +0530

    day07

---
+++ src/main/java/day07/practice/HashSetPractice.java
@@ -0,0 +1,30 @@
+package day07.practice;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class HashSetPractice {
+
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(2);
+
+		System.out.println("Using ArrayList :" + numbers);
+
+		
+		HashSet<Integer> numberset = new HashSet<>();
+		numberset.add(1);
+		numberset.add(2);
+		numberset.add(3);
+		numberset.add(2);
+
+		System.out.println("Using Set :" + numberset);
+		
+	}
+
+}
